f16edd4fd35cee230415cca2eabd43ac05989d05
Alexandr.Tsaregorodtsev <Alexandr.Tsaregorodtsev@nat.kz>
2017-09-18 23:30:59 +0300

    Proj files

---
+++ Framework/FT.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FT.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Project/SmartEducation.Public/SmartEducation.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SmartEducation.Data
+{
+    public class Class1
+    {
+    }
+}

a41888a8309385a6fa1731575eb7b4b59ff073db
Alexandr.Tsaregorodtsev <Alexandr.Tsaregorodtsev@nat.kz>
2017-10-08 00:58:46 +0300

    Identity Server init

---
+++ IdentityServer/Core.IdentityServer/Configs/IdentityConfig.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace Core.IdentityServer.Configs
+{
+    public static class IdentityConfig
+    {
+        public static List<TestUser> GetTestUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username =  "Frank",
+                    Password = "pwd",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Frank"),
+                        new Claim("family_name", "Underwook")
+                    }
+                },
+
+				new TestUser
+				{
+					SubjectId = "2",
+					Username =  "Claire",
+					Password = "pwd",
+
+					Claims = new List<Claim>
+					{
+						new Claim("given_name", "Claire"),
+						new Claim("family_name", "Underwook")
+					}
+				}
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+
+            };
+        }
+    }
+}
---
+++ IdentityServer/Core.IdentityServer/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Core.IdentityServer.Configs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.IdentityServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddIdentityServer()
+                    .AddDeveloperSigningCredential()
+                    .AddTestUsers(IdentityConfig.GetTestUsers())
+                    .AddInMemoryIdentityResources(IdentityConfig.GetIdentityResources())
+                    .AddInMemoryClients(IdentityConfig.GetClients());
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+			app.UseStaticFiles();
+			app.UseMvcWithDefaultRoute();
+        }
+    }
+}
